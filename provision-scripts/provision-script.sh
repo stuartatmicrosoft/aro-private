dde159e5ba237f20b0312655188da27adfbe083c
Stuart R. Kirk <stuart.kirk@microsoft.com>
2021-01-11 20:34:49 -0500

    Jumpbox Update

--- provision-scripts/provision-script.sh
+++ provision-scripts/provision-script.sh
@@ -17,10 +17,10 @@ echo "**************************************************************************
 	echo "`date`" >>/root/provision-script-output.log
 echo "********************************************************************************************"
 	echo "`date` -- Setting Student User password to 'Microsoft'" >>/root/provision-script-output.log
-	echo "Microsoft" | passwd --stdin student
+	echo "Microsoft" | passwd --stdin aroadmin
 echo "********************************************************************************************"
-	echo "`date` -- Adding student to wheel group for sudo access'" >>/root/provision-script-output.log
-	usermod -G wheel student
+	echo "`date` -- Adding aroadmin to wheel group for sudo access'" >>/root/provision-script-output.log
+	usermod -G wheel aroadmin
 echo "********************************************************************************************"
 	echo "`date` -- Setting Root Password to 'Microsoft'" >>/root/provision-script-output.log
 	echo "Microsoft" | passwd --stdin root
@@ -44,7 +44,7 @@ echo "**************************************************************************
         yum -y remove rhn-check rhn-client-tools rhn-setup rhnlib rhnsd yum-rhn-plugin subscription-manager >> /root/yum-output.log
         echo "FULL UPDATE" >> /root/yum-output.log
         yum -y update >> /root/yum-output.log
-        echo "student ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+        echo "aroadmin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
         alternatives --set python /usr/bin/python2
         cd /usr/bin
         curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
@@ -61,22 +61,22 @@ echo "**************************************************************************
 	systemctl enable iptables
 	systemctl start iptables	
 echo "********************************************************************************************"
-        echo "`date` -- Upgrading PIP and installing Ansible" >>/root/provision-script-output.log
-        runuser -l student -c "pip-2.7 install --upgrade --user python-dateutil"
-        runuser -l student -c "pip-2.7 install --upgrade --user openshift"
-        runuser -l student -c "pip-2.7 install --upgrade --user requests"
-        runuser -l student -c "pip-2.7 install --upgrade --user xmltodict"
-        runuser -l student -c "pip-2.7 install --upgrade --user pyOpenSSL"
-        runuser -l student -c "pip-2.7 install --upgrade --user podman"
-        runuser -l student -c "pip-2.7 install --user ansible==2.9.10"
-        pip-2.7 install --upgrade selinux
-        find /usr/lib/python2.7/site-packages -type f -exec chmod 644 {} +
-        find /usr/lib/python2.7/site-packages -type d -exec chmod 755 {} +
-        echo "[defaults]" > /home/student/.ansible.cfg
-        echo "no_log = True" >> /home/student/.ansible.cfg
-        echo "[ssh_connection]" >> /home/student/.ansible.cfg
-        echo "ssh_args = -o StrictHostKeyChecking=no" >> /home/student/.ansible.cfg
-        chown student:student /home/student/.ansible.cfg
+#        echo "`date` -- Upgrading PIP and installing Ansible" >>/root/provision-script-output.log
+#        runuser -l aroadmin -c "pip-2.7 install --upgrade --user python-dateutil"
+#        runuser -l aroadmin -c "pip-2.7 install --upgrade --user openshift"
+#        runuser -l aroadmin -c "pip-2.7 install --upgrade --user requests"
+#        runuser -l aroadmin -c "pip-2.7 install --upgrade --user xmltodict"
+#        runuser -l aroadmin -c "pip-2.7 install --upgrade --user pyOpenSSL"
+#        runuser -l aroadmin -c "pip-2.7 install --upgrade --user podman"
+#        runuser -l aroadmin -c "pip-2.7 install --user ansible==2.9.10"
+#        pip-2.7 install --upgrade selinux
+#        find /usr/lib/python2.7/site-packages -type f -exec chmod 644 {} +
+#        find /usr/lib/python2.7/site-packages -type d -exec chmod 755 {} +
+#        echo "[defaults]" > /home/aroadmin/.ansible.cfg
+#        echo "no_log = True" >> /home/aroadmin/.ansible.cfg
+#        echo "[ssh_connection]" >> /home/aroadmin/.ansible.cfg
+#        echo "ssh_args = -o StrictHostKeyChecking=no" >> /home/aroadmin/.ansible.cfg
+#        chown aroadmin:aroadmin /home/aroadmin/.ansible.cfg
 echo "********************************************************************************************"	
 	echo "`date` -- Installing the Azure Linux CLI" >>/root/provision-script-output.log
 	rpm --import https://packages.microsoft.com/keys/microsoft.asc
@@ -94,16 +94,16 @@ echo "**************************************************************************
         cd /usr/local
         tar xvfz v1.1.0.tar.gz
         ln -s /usr/local/noVNC-1.1.0/vnc.html /usr/local/noVNC-1.1.0/index.html
-        wget --quiet -P /etc/systemd/system https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/websockify.service
-	wget --quiet --no-check-certificate -P /etc/systemd/system "https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/vncserver@:4.service"
-	openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/novnc.pem -out /etc/pki/tls/certs/novnc.pem -days 365 -subj "/C=US/ST=Michigan/L=Ann Arbor/O=RedHatSummit/OU=CloudNativeAzure/CN=microsoft.com"
-	su -c "mkdir .vnc" - student
-	wget --quiet --no-check-certificate -P /home/student/.vnc https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/passwd
-	wget --quiet --no-check-certificate -P /home/student/.vnc https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/xstartup
-        chown student:student /home/student/.vnc/passwd
-        chown student:student /home/student/.vnc/xstartup
-        chmod 600 /home/student/.vnc/passwd
-        chmod 755 /home/student/.vnc/xstartup
+        wget --quiet -P /etc/systemd/system https://raw.githubusercontent.com/stuartatmicrosoft/aro-private/master/provision-scripts/websockify.service
+	wget --quiet --no-check-certificate -P /etc/systemd/system "https://raw.githubusercontent.com/stuartatmicrosoft/aro-private/master/provision-scripts/vncserver@:4.service"
+	openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/pki/tls/certs/novnc.pem -out /etc/pki/tls/certs/novnc.pem -days 365 -subj "/C=US/ST=Michigan/L=Ann Arbor/O=AROPrivate/OU=CloudNativeAzure/CN=microsoft.com"
+	su -c "mkdir .vnc" - aroadmin
+	wget --quiet --no-check-certificate -P /home/aroadmin/.vnc https://raw.githubusercontent.com/stuartatmicrosoft/aro-private/master/provision-scripts/passwd
+	wget --quiet --no-check-certificate -P /home/aroadmin/.vnc https://raw.githubusercontent.com/stuartatmicrosoft/aro-private/master/provision-scripts/xstartup
+        chown aroadmin:aroadmin /home/aroadmin/.vnc/passwd
+        chown aroadmin:aroadmin /home/aroadmin/.vnc/xstartup
+        chmod 600 /home/aroadmin/.vnc/passwd
+        chmod 755 /home/aroadmin/.vnc/xstartup
 	iptables -I INPUT 1 -m tcp -p tcp --dport 6080 -j ACCEPT
 	service iptables save
         chmod 644 /etc/systemd/system/pm2-root.service
@@ -115,66 +115,42 @@ echo "**************************************************************************
         systemctl start vncserver@:4.service
 	systemctl start websockify.service
 echo "********************************************************************************************"
-	echo "`date` -- Editing student's .bashrc and disabling Red Hat alerts" >> /root/provision-script-output.log
-	echo " " >> /home/student/.bashrc
-        echo "# Azure Service Principal Credentials" >> /home/student/.bashrc
-	echo "export AZURE_CLIENT_ID=$SP_APP_ID" >> /home/student/.bashrc
-	echo "export AZURE_SECRET=$SP_SECRET" >> /home/student/.bashrc
-	echo "export AZURE_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID" >> /home/student/.bashrc
-	echo "export AZURE_TENANT=$AZ_TENANT_ID" >> /home/student/.bashrc
-        su -c "gconftool-2 -t bool -s /apps/rhsm-icon/hide_icon true" - student
-	su -c "ssh-keygen -t rsa -q -P '' -f /home/student/.ssh/id_rsa" - student
-        mkdir -p /home/student/.local/share/keyrings
-	wget --quiet -P /home/student/.local/share/keyrings https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/Default.keyring
-        chown student:student /home/student/.local/share/keyrings/Default.keyring
-        restorecon /home/student/.local/share/keyrings/Default.keyring
+#	echo "`date` -- Editing aroadmin's .bashrc and disabling Red Hat alerts" >> /root/provision-script-output.log
+#	echo " " >> /home/aroadmin/.bashrc
+#        echo "# Azure Service Principal Credentials" >> /home/aroadmin/.bashrc
+#	echo "export AZURE_CLIENT_ID=$SP_APP_ID" >> /home/aroadmin/.bashrc
+#	echo "export AZURE_SECRET=$SP_SECRET" >> /home/aroadmin/.bashrc
+#	echo "export AZURE_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID" >> /home/aroadmin/.bashrc
+#	echo "export AZURE_TENANT=$AZ_TENANT_ID" >> /home/aroadmin/.bashrc
+        su -c "gconftool-2 -t bool -s /apps/rhsm-icon/hide_icon true" - aroadmin
+	su -c "ssh-keygen -t rsa -q -P '' -f /home/aroadmin/.ssh/id_rsa" - aroadmin
+        mkdir -p /home/aroadmin/.local/share/keyrings
+	wget --quiet -P /home/aroadmin/.local/share/keyrings https://raw.githubusercontent.com/stuartatmicrosoft/aro-private/master/provision-scripts/Default.keyring
+        chown aroadmin:aroadmin /home/aroadmin/.local/share/keyrings/Default.keyring
+        restorecon /home/aroadmin/.local/share/keyrings/Default.keyring
 echo "********************************************************************************************"
-        wget -P /etc/yum.repos.d https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/mongodb-org-4.2.repo 
-        yum -y install mongodb-org 
-        npm install pm2@latest -g
-        systemctl enable mongod
-        systemctl start mongod
-        iptables -I INPUT 2 -m tcp -p tcp --dport 80 -j ACCEPT
-        export MONGO_DBCONNECTION="mongodb://localhost:27017/nodejs-todo"
-        mkdir -p /source/sample-apps/nodejs-todo/src
-        cd /source/sample-apps/nodejs-todo/src   
-        git clone https://github.com/stuartatmicrosoft/nodejs-todo .
-        chown -R student:student /source
-        npm install
-        sed -i "s/8080/80/g" /source/sample-apps/nodejs-todo/src/server.js
-        pm2 start server.js
-        pm2 save
-        pm2 startup systemd -u root
-        systemctl start pm2-root
-        service iptables save
-echo "********************************************************************************************"
-        chown -R student:student /home/student/.local
-        chmod a+rx /home/student/.local
+        chown -R aroadmin:aroadmin /home/aroadmin/.local
+        chmod a+rx /home/aroadmin/.local
         cd /usr/local/bin
-        wget -P /usr/local/bin https://raw.githubusercontent.com/stuartatmicrosoft/RedHatSummit2020/master/provision-scripts/oc.tar.gz
+        wget -P /usr/local/bin https://raw.githubusercontent.com/stuartatmicrosoft/aro-private/master/provision-scripts/oc.tar.gz
         tar xvfz oc.tar.gz
         rm -f oc.tar.gz
-echo "********************************************************************************************"
-        echo "`date` -- Installing MSSQL Tools" >>/root/provision-script-output.log
-        curl https://packages.microsoft.com/config/rhel/8/prod.repo > /etc/yum.repos.d/msprod.repo
-        ACCEPT_EULA=Y yum install -y mssql-tools unixODBC-devel
-        echo "PATH=$PATH:/opt/mssql-tools/bin" >> /etc/profile
 echo "********************************************************************************************"
 
 echo "`date` --END-- Provisioning" >>/root/provision-script-output.log
 
 echo "`date` Creating Student Desktop Credentials File" >>/root/provision-script-output.log
 
-mkdir /home/student/Desktop
-chown student:student /home/student/Desktop
-echo AZURE_USER_NAME=$AZ_USER_NAME >> /home/student/Desktop/credentials.txt
-echo AZURE_USER_PASSWORD=$AZ_USER_PASSWORD >> /home/student/Desktop/credentials.txt
-echo AZURE_CLIENT_ID=$SP_APP_ID >> /home/student/Desktop/credentials.txt
-echo AZURE_SECRET=$SP_SECRET >> /home/student/Desktop/credentials.txt
-echo AZURE_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID >> /home/student/Desktop/credentials.txt
-echo AZURE_TENANT_ID=$AZ_TENANT_ID >> /home/student/Desktop/credentials.txt
-echo GUIDE_URL=https://github.com/stuartatmicrosoft/RedHatSummit2020 >> /home/student/Desktop/credentials.txt
-chown student:student /home/student/Desktop/credentials.txt
+mkdir /home/aroadmin/Desktop
+chown aroadmin:aroadmin /home/aroadmin/Desktop
+echo AZURE_USER_NAME=$AZ_USER_NAME >> /home/aroadmin/Desktop/credentials.txt
+echo AZURE_USER_PASSWORD=$AZ_USER_PASSWORD >> /home/aroadmin/Desktop/credentials.txt
+echo AZURE_CLIENT_ID=$SP_APP_ID >> /home/aroadmin/Desktop/credentials.txt
+echo AZURE_SECRET=$SP_SECRET >> /home/aroadmin/Desktop/credentials.txt
+echo AZURE_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID >> /home/aroadmin/Desktop/credentials.txt
+echo AZURE_TENANT_ID=$AZ_TENANT_ID >> /home/aroadmin/Desktop/credentials.txt
+echo GUIDE_URL=https://github.com/stuartatmicrosoft/aro-private >> /home/aroadmin/Desktop/credentials.txt
+chown aroadmin:aroadmin /home/aroadmin/Desktop/credentials.txt
 
 echo "`date` --END-- Provision Script" >>/root/provision-script-output.log
 
